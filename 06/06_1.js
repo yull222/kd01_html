5574d3b9fda87c2844df2792a650f981f29d50df
yull222 <yurilee4942@gmail.com>
2025-02-14 10:55:28 +0900

    수정

---
+++ 06/06_1.js
@@ -0,0 +1,28 @@
+function lotto() {
+    //1. 로또번호생성
+    //1-1. 로또배열 
+    let arrLotto = [] ;
+  
+    //1-2. 6개
+    while( arrLotto.length < 6 ) {
+      let n = Math.floor(Math.random() * 45) + 1 ; // 1~45까지 
+  
+      if (!arrLotto.includes(n)) arrLotto.push(n) ;
+    }
+    //1-2. 6개 번호 정렬
+    arrLotto.sort((a, b) => a - b) ;
+  
+    //1-3.  + 1개 번호 추출
+    while( arrLotto.length < 7 ) {
+      let n = Math.floor(Math.random() * 45) + 1 ; // 1~45까지 
+  
+      if (!arrLotto.includes(n)) arrLotto.push(n) ;
+    }
+  
+    console.log(arrLotto) ;
+  
+    let spanTgs  = arrLotto.map(item => `<span class ='sp${Math.floor(item/10)}'> ${item}</span>`)
+                            .join('');
+    console.log(spanTgs) ;
+    document.getElementById("msg").innerHTML = spanTgs;
+  }
